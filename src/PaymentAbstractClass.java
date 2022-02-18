d2c18bd6022c0725d4009de4f9acba4af56ef5e3
Samuel Iwuchukwu <samueli@ng.neolife.com>
2022-02-18 19:43:23 +0100

    added the abstract classes for updating status

--- src/PaymentAbstractClass.java
+++ src/PaymentAbstractClass.java
@@ -1,4 +1,6 @@
 public abstract class PaymentAbstractClass {
 
     public abstract int DeductCharges(float taxRate);
+    public abstract String UpdateStatus(String response, int AccountType,double amount, int pin);
+
 }
